66d253826b85bbbd2609751fc521a140eae29cf1
Nick Saunders <nick@saunde.rs>
2021-04-24 16:45:23 -0700

    Zephyr customizations

--- lib/utils.bash
+++ lib/utils.bash
@@ -13,7 +13,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if zephyr is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -26,12 +25,10 @@ sort_versions() {
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
     grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    sed 's/^v//'
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if zephyr has other means of determining installable versions.
   list_github_tags
 }
 
@@ -48,7 +45,6 @@ download_release() {
     fail "unrecognized operating system $OSTYPE"
   fi
 
-  # TODO: Adapt the release URL convention for zephyr
   url="$GH_REPO/releases/download/v${version}/${bin}.tar.gz"
 
   echo "* Downloading $TOOL_NAME release $version..."
@@ -65,12 +61,10 @@ install_version() {
   fi
 
   (
-    mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH/*" "$install_path"
+    mkdir -p "$install_path/bin"
 
-    ls $install_path
+    cp "$ASDF_DOWNLOAD_PATH/zephyr" "$install_path/bin"
 
-    # TODO: Asert zephyr executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
